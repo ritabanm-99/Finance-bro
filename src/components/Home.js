73246112f071f0024892e1a29c49437ac542c561
Ritaban Mitra <115375296+ritabanm-99@users.noreply.github.com>
2024-04-28 10:56:02 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,20 @@ const Home = () => {
   return (
     <div className="card-container">
       <Link to="/investments">
-        <Card title="Investments" content="Investing Analytics." />
+        <Card title="Investments" content="Equity & Options Research." />
       </Link>
       <Link to="/savings">
         <Card title="Savings" content="Savings Analytics." />
+      </Link>
+      <Link to = "/wealthmgmt">
+        <Card title = "Wealth Management" content= "Wealth Management"/>
+      </Link>
+      <Link to = "/Insurance">
+        <Card title = "Insurance" content = " Learn about your Insurance benefits"/>
       </Link>
     </div>
   );
 };
 
 export default Home;
+
