bc7a43f2bd378f8edcd02fdee3af73b330bd8e7b
Ritaban Mitra <115375296+ritabanm-99@users.noreply.github.com>
2024-04-28 11:07:55 -0400

    Add files via upload

---
+++ src/Insurance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Insurance = () => {
+  return (
+    <div>
+      <h1>Insurance</h1>
+    </div>
+  );
+};
+
+export default Insurance;
---
+++ src/Wealth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Wealth = () => {
+  return (
+    <div>
+      <h1>Wealth Management Chatbot</h1>
+    </div>
+  );
+};
+
+export default Wealth;
